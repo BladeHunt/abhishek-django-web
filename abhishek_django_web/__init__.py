23b3d5762c576baf9679cc4a177d1edce135b099
Abhishek Pradhannag <abhishek.pradhanang@outlook.com>
2021-02-20 15:05:11 +1100

    Add project files.

---
+++ abhishek_django_web/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for abhishek_django_web.
+"""
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\pradh\source\repos\abhishek-django-web\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
